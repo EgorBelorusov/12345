b4e82e00815bcf7d9e82512d6e1b7f56b4ff5d44
Белорусов Егор Васильевич <beev@college.spk>
2021-05-18 12:04:07 +0700

    egorka

---
+++ app.js
@@ -0,0 +1,20 @@
+let header = document.querySelector('header');
+console.log(header);
+let conteiner = document.querySelector('.conteiner');
+console.log(conteiner);
+let nav = document.querySelector('.nav');
+console.log(nav);
+let logo = document.querySelector('.logo');
+console.log(logo);
+
+
+logo.style.color = 'green';
+logo.style.fontSize = '30px';
+logo.style.width = '90px';
+logo.style.hight = '90px';
+logo.style.backgroundColor = 'blue';
+logo.style.textAlign = 'center';
+logo.style.borderLeft = '3px solid red';
+logo.style.borderRight = '3px solid red';
+logo.style.borderBottom = '3px solid red';
+logo.style.borderTop = '3px solid red';
